05eef68d1f9ea6ca4658eae0b460872f738723e5
developertipu <70166545+developertipu@users.noreply.github.com>
2020-09-20 21:30:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+ $('button').click(function(){
+     
+     $('div').animate({
+         
+         left:'300px',
+         width:'150px',
+         height:'150px'
+         
+         
+         
+            });
+       });
+    });
